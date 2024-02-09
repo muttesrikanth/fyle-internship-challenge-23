c90ab087a26b783c8d9149688c469ebe467c58e4
srikanthmmsys <msveerabramham@msystechnologies.com>
2024-02-09 23:16:36 +0530

    service file updated

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +14,7 @@ export class ApiService {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string){
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`);
+  }
 }
